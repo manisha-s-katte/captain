10bdea905cd1e97f424862d37ac575cde6b452ec
Dhruv Rekhawat <110072380+DhruvRekhawat@users.noreply.github.com>
2024-09-25 16:20:43 +0530

    carousel

--- src/app/gamePass/page.tsx
+++ src/app/gamePass/page.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from '@/components/landing/Navbar'
+import GamePassCarousel from '@/components/GamePass/Carousel'
 import CategoryGamePass from '@/components/GamePass/CategoryGamePass'
 import GamePassFooter from '@/components/GamePass/GamePassFooter'
 
 const gamePass = () => {
   return (
-    <main className='bg-gradient-to-tl from-[#110219] to-[#37024F]' >
+    <main className='bg-gradient-to-tl from-[#110219] to-[#37024F] text-white' >
       <Navbar bgColor='transparent' />
+      <div className='w-full flex justify-center items-center'>
+    <GamePassCarousel></GamePassCarousel>
+    </div>
       <CategoryGamePass />
       <GamePassFooter />
     </main>
--- src/components/GamePass/Carousel.tsx
+++ src/components/GamePass/Carousel.tsx
@@ -1,12 +1,50 @@
-import { Carousel } from '@mantine/carousel';
+import React from 'react'
+import { Card, CardContent } from "../ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import Image from 'next/image'
+import GP1 from '@/assets/Images/GamePass/GP1.jpeg'
+import { Button } from '../ui/button'
 
-function Demo() {
+
+const GamePassCarousel = () => {
   return (
-    <Carousel withIndicators height={200}>
-      <Carousel.Slide>1</Carousel.Slide>
-      <Carousel.Slide>2</Carousel.Slide>
-      <Carousel.Slide>3</Carousel.Slide>
-      {/* ...other slides */}
-    </Carousel>
-  );
+    <Carousel className="w-full max-w-[50%]">
+    <CarouselContent className=''>
+      {Array.from({ length: 5 }).map((_, index) => (
+        <CarouselItem key={index}>
+          <div className="p-1 bg-transparent">
+            <Card className='bg-transparent border-none max-w-[1290px] h-[450px] flex justify-center items-center'>
+              <CardContent className="flex aspect-video p-0 justify-center items-center gap-4 ">
+                <div className='overflow-clip w-[70%] aspect-video'>
+                <Image src={GP1} alt='' className='object-cover hover:scale-125 transition-all'></Image>
+                </div>
+                <div className='flex flex-col items-start'>
+                  <h2 className=' text-xl uppercase font-extrabold text-white'>
+                    Fragger
+                  </h2>
+                  <p className='text-purple-500 text-sm'>
+                    (INR 4500)
+                  </p>
+                  <Button className='w-full rounded-none mt-4 bg-[#D600E1]'>Explore</Button>
+
+                  
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+        </CarouselItem>
+      ))}
+    </CarouselContent>
+    <CarouselPrevious />
+    <CarouselNext />
+  </Carousel>
+  )
 }
+
+export default GamePassCarousel
