84af65dff8247ded2828b33eea0b633d3dd53109
Lovish Duggal <lovishduggal11@gmail.com>
2024-09-28 17:37:14 +0530

    feat: implement game pass carousel with dynamic data

--- src/app/gamePass/page.tsx
+++ src/app/gamePass/page.tsx
@@ -1,20 +1,56 @@
-import React from 'react'
-import Navbar from '@/components/landing/Navbar'
-import GamePassCarousel from '@/components/GamePass/Carousel'
-import CategoryGamePass from '@/components/GamePass/CategoryGamePass'
-import GamePassFooter from '@/components/GamePass/GamePassFooter'
+'use client';
+import React, { useEffect, useState } from 'react';
+import Navbar from '@/components/landing/Navbar';
+import GamePassCarousel from '@/components/GamePass/Carousel';
+import CategoryGamePass from '@/components/GamePass/CategoryGamePass';
+import GamePassFooter from '@/components/GamePass/GamePassFooter';
+import { createClient } from '@/lib/supabase/client';
+import Spinner from '@/components/Spinner/spinner';
 
-const gamePass = () => {
+const GamePass = () => {
+  const [gamePasses, setGamePasses] = useState<Record<string, any>[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const fetchGamePasses = async () => {
+      setIsLoading(true);
+      const supabase = createClient();
+      const { data, error } = await supabase
+        .from('GamePass')
+        .select('*')
+        .eq('status', 'active');
+
+      if (error) {
+        console.error('Error fetching game passes:', error);
+      } else {
+        setGamePasses(data || []);
+      }
+      setIsLoading(false);
+    };
+
+    fetchGamePasses();
+  }, []);
   return (
-    <main className='bg-gradient-to-tl from-[#110219] to-[#37024F] text-white' >
-      <Navbar bgColor='transparent' />
-      <div className='w-full flex justify-center items-center'>
-    <GamePassCarousel></GamePassCarousel>
-    </div>
-      <CategoryGamePass />
-      <GamePassFooter />
+    <main className="bg-gradient-to-tl from-[#110219] to-[#37024F] text-white min-h-screen">
+      <Navbar bgColor="transparent" />
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <>
+          {gamePasses?.length === 0 ? (
+            <p className="text-white text-center mt-32">No game passes</p>
+          ) : (
+            <>
+              <div className="w-full flex justify-center items-center">
+                <GamePassCarousel gamePasses={gamePasses} />
+              </div>
+              <CategoryGamePass />
+              <GamePassFooter />
+            </>
+          )}
+        </>
+      )}
     </main>
-  )
-}
+  );
+};
 
-export default gamePass
+export default GamePass;
